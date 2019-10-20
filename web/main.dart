988e640b4ff664f73d1c03052f0a8e081eb35480
duongthienlee <duongthienlee@gmail.com>
2019-10-20 19:57:04 +0300

    feat: todo list is addable

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,17 @@
 import 'dart:html';
+import 'package:uuid/uuid.dart';
+
+var uuid = Uuid();
 
 // Global var elements
+InputElement toDoInput = querySelector('#to-do-input');
+var addTodoBtn = querySelector('#add-todo');
 var modal_overlay = querySelector('.modal__overlay');
 var modal_content = querySelector('.modal__content');
+
 //Create test user
-User user = User("duongthienlee@gmail.com", "Thien Ly", "123456");
+var listTodo = ['Brushing teeth', 'Having breakfast', 'Go to school'];
+User user = User("duongthienlee@gmail.com", "Thien Ly", "123456", listTodo);
 
 void main() {
   // Open Welcome Modal
@@ -15,8 +22,11 @@ void main() {
   querySelector('#modal-btn-confirm')
       .onClick
       .listen(onClickConfirm); // login check
-}
 
+  addTodoBtn.onClick.listen(add_todo);
+
+  print("hahha ${user.get_listTodo}");
+}
 
 //------------------------------ Onclick event handlers
 void loginCheck(MouseEvent event) {
@@ -27,9 +37,10 @@ void loginCheck(MouseEvent event) {
   if (emailInput == user.get_email && pwdInput == user.get_pwd) {
     onCloseModal(event); // close modal
     // Todo list rendering
-    querySelector('#greeting').text = 'Hello ${user.get_name}, Things to do:';
+    querySelector('#greeting').text =
+        'Hello ${user.get_name}, Here are things to do:';
 
-    todoList = querySelector('#todolist');
+    todoListEle = querySelector('#todolist');
     user.get_todo_list;
   } else {
     outputInfo.classes.add("error");
@@ -48,43 +59,63 @@ void onClickConfirm(MouseEvent event) {
   loginCheck(event);
 }
 
-//------------------------------ Todo list -------------------------------------
-Iterable<String> thingsTodo() sync* {
-  var actions = ['Brushing teeth', 'Having breakfast', 'Go to school'];
+void add_todo(Event e) {
+  String inputValue = toDoInput.value;
+  if (inputValue != '') {
+    var newToDo = LIElement();
+    newToDo.text = inputValue;
+    user.add_new_todo(inputValue); // add to object User
+    todoListEle.children.add(newToDo); // render
+    toDoInput.value = ''; // reset input field
+  } else {
+    querySelector("#to-do-input-error").text = "Empty space is not allowed!";
+  }
+}
 
-  for (var action in actions) {
-    yield '$action ';
+//------------------------------ Todo list -------------------------------------
+Iterable<String> thingsTodo(listTodo) sync* {
+  for (var todo in listTodo) {
+    yield '$todo ';
   }
 }
 
 void addTodoItem(String item) {
-  print(item);
-
   var listElement = LIElement();
   listElement.text = item;
 
-  todoList.children.add(listElement);
+  todoListEle.children.add(listElement);
 }
 
-UListElement todoList;
+UListElement todoListEle;
 
 //------------------------------ User class ------------------------------------
 class User {
   //field
+  final String id = uuid.v1();
   String name;
   String email;
   String pwd;
+  List listTodo;
 
   //Dart Constructor
-  User(String email, String name, String pwd) {
+  User(String email, String name, String pwd, List listTodo) {
     this.email = email;
     this.name = name;
     this.pwd = pwd;
+    this.listTodo = listTodo;
   }
 
   //getters
+  String get get_id {
+    return this.id;
+  }
+
   void get get_todo_list {
-    thingsTodo().forEach(addTodoItem);
+    thingsTodo(listTodo).forEach(addTodoItem);
+  }
+
+  List get get_listTodo {
+    return this.listTodo;
   }
 
   String get get_email {
@@ -97,5 +128,9 @@ class User {
 
   String get get_name {
     return this.name;
+  }
+
+  void add_new_todo(String value) {
+    this.listTodo.add(value);
   }
 }
